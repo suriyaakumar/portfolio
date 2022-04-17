f70606049501a4bc470fa5d6a1a3b8424be0e7d7
suriyaakumar <believeinsurya@gmail.com>
2022-04-17 14:11:52 +0530

    replace for with foreach

--- script.js
+++ script.js
@@ -2,28 +2,38 @@ function myFunction()  {
     document.body.classList.toggle('light');
     document.body.classList.toggle('dark');
     var cards = document.getElementsByClassName('block');
-    for(var i=0; i< cards.length; i++){
-        cards[i].classList.toggle('neumorphism');
-        cards[i].classList.toggle('neumorphism-dark');
-    }
+    Array.from(cards).forEach(
+        i => {
+            i.classList.toggle('neumorphism');
+            i.classList.toggle('neumorphism-dark');
+        }
+    )
     var projects = document.getElementsByClassName('project');
-    for(var i=0; i< cards.length; i++){ 
-        projects[i].classList.toggle('inset');
-        projects[i].classList.toggle('inset-dark');
-    }
+    Array.from(projects).forEach(
+        i => {
+            i.classList.toggle('inset');
+            i.classList.toggle('inset-dark');
+        }
+    )
     var icons = document.getElementsByClassName('link');
-    for(var i=0; i< icons.length; i++){ 
-        icons[i].classList.toggle('icon');
-        icons[i].classList.toggle('icon-dark');
-    }
+    Array.from(icons).forEach(
+        i => {
+            i.classList.toggle('icon');
+            i.classList.toggle('icon-dark');
+        }
+    )
     var inputs = document.getElementsByClassName('form-input');
-    for(var i=0; i< inputs.length; i++){
-        inputs[i].classList.toggle('input');
-        inputs[i].classList.toggle('input-dark');
-    }
+    Array.from(inputs).forEach(
+        i => {
+            i.classList.toggle('input');
+            i.classList.toggle('input-dark');
+        }
+    )
     var button = document.getElementsByClassName('button');
-    for(var i=0; i<button.length; i++){
-        button[i].classList.toggle('neobutton');
-        button[i].classList.toggle('neobutton-dark');
-    } 
+    Array.from(button).forEach(
+        i => {
+            i.classList.toggle('neobutton');
+            i.classList.toggle('neobutton-dark');
+        }
+    )
 }           
